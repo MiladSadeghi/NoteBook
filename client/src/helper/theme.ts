30e611b24d1efeb9ff68ae2e3538bc53d88ba281
MiladSadeghi <miladsadeghi2323@gmail.com>
2023-04-11 07:43:53 +0330

    fix deploy problem

--- client/src/helper/theme.ts
+++ client/src/helper/theme.ts
@@ -1,45 +1,35 @@
 import { createTheme } from "@mui/material";
 const theme = createTheme({
   typography: {
-    fontFamily: '"Noto Sans", Roboto, Helvetica Neue, Arial',
-    fontWeightThin: 100,
-    fontWeightExtraLight: 200,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightSemiBold: 600,
-    fontWeightBold: 700,
-    fontWeightExtraBold: 800,
-    fontWeightBlack: 900,
     h1: {
       fontSize: "27px",
       lineHeight: "140%",
-      fontWeight: "600"
+      fontWeight: "600",
     },
     h2: {
       fontSize: "24px",
       lineHeight: "140%",
-      fontWeight: "600"
+      fontWeight: "600",
     },
     h3: {
       fontSize: "21px",
       lineHeight: "140%",
-      fontWeight: "600"
+      fontWeight: "600",
     },
     h4: {
       fontSize: "19px",
       lineHeight: "140%",
-      fontWeight: "600"
+      fontWeight: "600",
     },
     h5: {
       fontSize: "17px",
       lineHeight: "150%",
-      fontWeight: "500"
+      fontWeight: "500",
     },
     h6: {
       fontSize: "15px",
       lineHeight: "150%",
-      fontWeight: "500"
+      fontWeight: "500",
     },
   },
   direction: "rtl",
